66573f1b07944996ab20837c28068f933a24d844
Tree House <oredgaming@gmail.com>
2023-07-16 14:43:54 -0400

    Merged in to update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main>
 
       <Image src="/background_2.jpg" width={560} height={470} className='w-full h-auto blur-md absolute -z-10 brightness-50' priority={true} />
-      <h1 className={`pt-28 text-9xl bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-red-500 bg-pink-500 w-full text-center animate-shadow-pulse ${airstikeOutlineRegular .className}`}>COMP HAX</h1>
+      <h1 className={`pt-28 text-9xl bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-red-500 bg-pink-500 w-full text-center animate-shadow-pulse  ${airstikeOutlineRegular .className}`}>COMP HAX</h1>
 
       <h3 className='text-center text-xl text-white/70 mt-28'>A modern redesign of the GTAV mod menu</h3>
 
@@ -19,7 +19,12 @@ export default function Home() {
         <Link href={"/features"}><ButtonWhiteOutline>Features</ButtonWhiteOutline></Link>
       </div>
 
-
+      <div className='w-full flex flex-row justify-center'>
+        <div className='w-[65rem]'>
+          <h1 className=''>Why choose us?</h1>
+        </div>
+      </div>
+      
 
     </main>
   )
