b69b6550196f92ac46a0943d13b4606a8821f4c9
Tree House <oredgaming@gmail.com>
2023-07-16 15:30:21 -0400

    created login, added glow to index title

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main>
 
       <Image src="/background_2.jpg" width={560} height={470} className='w-full h-auto blur-md absolute -z-10 brightness-50' priority={true} />
-      <h1 className={`pt-28 text-9xl bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-red-500 bg-pink-500 w-full text-center animate-shadow-pulse  ${airstikeOutlineRegular .className}`}>COMP HAX</h1>
+      <h1 className={`pt-28 text-9xl bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-red-500 bg-pink-500 w-full text-center animate-shadow-pulse drop-shadow-[0_0px_5px_rgba(255,255,255,0.5)] ${airstikeOutlineRegular .className}`}>COMP HAX</h1>
 
       <h3 className='text-center text-xl text-white/70 mt-28'>A modern redesign of the GTAV mod menu</h3>
 
